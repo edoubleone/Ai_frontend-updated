1d436fca3d8b25fba9d5b89c9e2a579991355500
Sunday Solomon <nomolos2019@gmail.com>
2025-05-27 14:39:53 +0100

    Fix: Resolved build error concerning an unused import

--- src/Pages/.tsx
+++ src/Pages/.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 export default function Home() {
     return (
         <h1 className="text-3xl font-bold text-center mt-10 text-blue-600">
