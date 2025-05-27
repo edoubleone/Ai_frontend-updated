5dcf4525af1819fa1decdc7b50de1cda7cb8ceff
Sunday Solomon <nomolos2019@gmail.com>
2025-05-27 18:18:27 +0100

    Fix: Merging Changes from Adedayo

---
+++ src/Pages/dashboard/Landing.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from '@/components/bot/dashboardLayout'
+
+const Landing = () => {
+  return (
+    <main>
+        <DashboardLayout />
+    </main>
+  )
+}
+
+export default Landing
