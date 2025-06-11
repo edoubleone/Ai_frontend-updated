4791430f87eeea09ad55c9998a72a5027741e0a8
shadrxcc <akaadesm@gmail.com>
2025-06-11 13:58:33 +0100

    update calendar

--- src/components/shared/datepicker.tsx
+++ src/components/shared/datepicker.tsx
@@ -6,13 +6,6 @@ import { cn } from "../../lib/utils";
 import { Calendar } from "../ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Label } from "../ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
 
 interface SingleDatePickerProps extends React.HTMLAttributes<HTMLDivElement> {
   date?: Date | null;
@@ -38,34 +31,16 @@ export function DatePicker({
   optional = false,
   label,
   error,
+  errorText,
   dateFormat = "PPP",
   disabled,
   startDate,
-  errorText,
   ...props
 }: SingleDatePickerProps) {
   const [date, setDate] = React.useState<Date | undefined>(
     controlledDate ?? undefined
   );
   const [open, setOpen] = React.useState<boolean>(false);
-  const [month, setMonth] = React.useState<number>(new Date().getMonth());
-  const [year, setYear] = React.useState<number>(new Date().getFullYear());
-
-  const years = React.useMemo(() => {
-    const currentYear = new Date().getFullYear();
-    return Array.from({ length: 101 }, (_, i) => currentYear - i);
-  }, []);
-
-  const months = React.useMemo(() => {
-    return Array.from({ length: 12 }, (_, i) => new Date(year, i, 1));
-  }, [year]);
-
-  React.useEffect(() => {
-    if (date) {
-      setMonth(date.getMonth());
-      setYear(date.getFullYear());
-    }
-  }, [date]);
 
   React.useEffect(() => {
     setDate(controlledDate ?? undefined);
@@ -77,35 +52,10 @@ export function DatePicker({
     setOpen(false);
   };
 
-  const handleYearChange = (selectedYear: string) => {
-    const newYear = parseInt(selectedYear, 10);
-    setYear(newYear);
-    if (date) {
-      const newDate = new Date(date);
-      newDate.setFullYear(newYear);
-      setDate(newDate);
-    }
-  };
-
-  const handleMonthChange = (selectedMonth: string) => {
-    const newMonth = parseInt(selectedMonth, 10);
-    setMonth(newMonth);
-    if (date) {
-      const newDate = new Date(date);
-      newDate.setMonth(newMonth);
-      setDate(newDate);
-    } else {
-      setDate(new Date(year, newMonth, 1));
-    }
-  };
-
   return (
     <div className={cn("grid w-full gap-2", className)} {...props}>
       <div className="flex justify-between">
-        <Label
-          className="text-base inline-flex items-center gap-x-1.5 text-[#454545] font-semibold"
-          htmlFor={label}
-        >
+        <Label className="text-base inline-flex items-center gap-x-1.5 text-[#454545] font-semibold">
           {label}
         </Label>
         {optional && <Label htmlFor={label}>Optional</Label>}
@@ -126,44 +76,13 @@ export function DatePicker({
           </button>
         </PopoverTrigger>
         <PopoverContent className="w-auto !z-9999 p-0">
-          <div className="flex justify-between p-2 space-x-1">
-            <Select onValueChange={handleYearChange} value={year.toString()}>
-              <SelectTrigger className="w-[120px]">
-                <SelectValue placeholder="Year" />
-              </SelectTrigger>
-              <SelectContent>
-                {years.map((y) => (
-                  <SelectItem key={y} value={y.toString()}>
-                    {y}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <Select onValueChange={handleMonthChange} value={month.toString()}>
-              <SelectTrigger className="w-[120px]">
-                <SelectValue placeholder="Month" />
-              </SelectTrigger>
-              <SelectContent>
-                {months.map((m, index) => (
-                  <SelectItem key={index} value={index.toString()}>
-                    {format(m, "MMMM")}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-
           <Calendar
-            defaultMonth={new Date(year, month)}
-            onMonthChange={(newMonth: Date) => {
-              setMonth(newMonth.getMonth());
-              setYear(newMonth.getFullYear());
-            }}
             mode="single"
+            captionLayout="dropdown"
             selected={date}
             onSelect={handleDateChange}
-            initialFocus
             disabled={startDate ? (day: Date) => day < startDate : disabled}
+            className="rounded-lg border shadow-sm"
           />
         </PopoverContent>
       </Popover>
